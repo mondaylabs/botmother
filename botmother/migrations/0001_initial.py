fb49315dd361ad3c5ca41a5f92932d6cfe20f511
Саидравшан <shakirov.saidravshan@gmail.com>
2020-11-26 11:25:37 +0500

    make migrations swappable

--- botmother/migrations/0001_initial.py
+++ botmother/migrations/0001_initial.py
@@ -11,6 +11,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
+        migrations.swappable_dependency(settings.BOTMOTHER_CHAT_MODEL),
     ]
 
     operations = [

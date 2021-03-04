53b800b5284cef12c6f4043de0994f87e0839c8a
Odilbek Arziev <76895495+Odilbek-Arziev@users.noreply.github.com>
2021-03-04 19:15:55 +0500

    Update urls.py

--- example/urls.py
+++ example/urls.py
@@ -5,8 +5,14 @@ from example.handlers import main
 
 def dispatch(router):
     router.command('/start', main.start, extra={'back': True})
+    router.callback('is-chosen', main.last)
+
+    # router.text(None, main.change, last_action=main.start)
+
+    router.any(main.unknown)
 
 
 urlpatterns = [
-    path('', webhook(dispatch)),
+    path('webhook', webhook(dispatch)),
 ]
+

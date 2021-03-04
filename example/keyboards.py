092ea94003fe444cc998c6b30db684c3af6638fc
Odilbek Arziev <76895495+Odilbek-Arziev@users.noreply.github.com>
2021-03-04 19:13:52 +0500

    Update keyboards.py

---
+++ example/keyboards.py
@@ -0,0 +1,16 @@
+from botmother.utils.keyboards import *
+
+
+def main_menu():
+    return keyboard([
+        [button('Да'), button('Нет')],
+        [button('Не знаю')]
+    ])
+
+
+def inline_menu(*args, **kwargs):
+    return inline_keyboard([
+        [inline('I am fine!', {'value': True}, 'is-chosen')],
+        [inline('I am sad!', {'value': False}, 'is-chosen')]
+    ])
+
